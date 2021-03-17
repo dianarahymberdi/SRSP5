4665c77deecd39424ca27b425082facd763f01ad
Diana <diana02rahimberdi@gmail.com>
2021-03-17 10:10:46 +0600

    Добавьте файлы проекта.

---
+++ SRSP5/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace SRSP5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b;
+            Console.Write(" a = ");
+            a = Convert.ToInt32(Console.ReadLine());
+            Console.Write(" b = ");
+            b = Convert.ToInt32(Console.ReadLine());
+            for (int i = a; i <= b; i++)
+            {
+                Console.WriteLine(i);
+            }
+            Console.ReadKey();
+        }
+    }
+}
